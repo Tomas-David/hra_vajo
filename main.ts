4b88503b00723332a41c49a6a6882013ed3ab390
Tomáš David <115162566+Tomas-David@users.noreply.github.com>
2023-03-01 17:54:28 +0100

    zmenšní kodu

--- main.ts
+++ main.ts
@@ -1,52 +1,43 @@
 let x = 0
 let y = 0
 let z = 0
-// x tolerance 100
-// 
-// y tolerance 150
+let gameOver = false
+
 basic.forever(function () {
-    
-    input.setAccelerometerRange(AcceleratorRange.TwoG)
-    x = input.acceleration(Dimension.X)
-    y = input.acceleration(Dimension.Y)
-    z = input.acceleration(Dimension.Z)
-    pause(100)
-basic.showLeds(`
+
+    input.onButtonPressed(Button.A, function () {
+        gameOver = false
+    })
+
+    while (gameOver != true) {
+        input.setAccelerometerRange(AcceleratorRange.TwoG)
+        x = input.acceleration(Dimension.X)
+        y = input.acceleration(Dimension.Y)
+        z = input.acceleration(Dimension.Z)
+        pause(100)
+        let sum = x + y + z
+
+
+        basic.showLeds(`
         . . . . .
         . . . . .
         . . # . .
         . . . . .
         . . . . .
         `)
-    if (x >= 100 || x <= -100) {
-        music.playTone(262, music.beat(BeatFraction.Whole))
-        basic.showLeds(`
+        if (sum >= -500 || sum <= -1200) {
+            music.playTone(262, music.beat(BeatFraction.Whole))
+            basic.showLeds(`
             # . . . #
             . # . # .
             . . # . .
             . # . # .
             # . . . #
             `)
+            gameOver = true
+        }
+
+        basic.pause(100)
     }
-    if (y >= 150 || y <= -150) {
-        music.playTone(262, music.beat(BeatFraction.Whole))
-        basic.showLeds(`
-            # . . . #
-            . # . # .
-            . . # . .
-            . # . # .
-            # . . . #
-            `)
-    }
-    if (z > 0) {
-        music.playTone(262, music.beat(BeatFraction.Whole))
-        basic.showLeds(`
-            # . . . #
-            . # . # .
-            . . # . .
-            . # . # .
-            # . . . #
-            `)
-    }
-    basic.pause(100)
+
 })
